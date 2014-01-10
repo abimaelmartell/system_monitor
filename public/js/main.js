4705dfb88a0e6638983d3ecf9540cb6d78bb83fc
Abimael Martell <abimex@gmail.com>
2014-01-09 16:17:04 -0800

    add assets

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+(function($, window, undefined){
+  var SystemStats = function(){
+    var stats_json_url = "/stats.json";
+    this.init = function(){
+    }
+
+    this.fetchStats = function(){
+    }
+
+    this.renderStats = function(){
+    }
+
+    return this.init();
+  };
+
+
+
+  window.system_stats = new SystemStats;
+})(Zepto, window);
