563af1e30b4e8266bcb061da21fd421c205be7c1
Abimael Martell <abimex@gmail.com>
2014-01-10 16:55:52 -0800

    fix int type to int64

--- src/web.c
+++ src/web.c
@@ -9,7 +9,7 @@ void initialize_server(void){
   mg_set_option(server, "listening_port", DEFAULT_HTTP_PORT);
   mg_set_option(server, "document_root", "public");
 
-  mg_add_uri_handler(server, "/stats/all.json", &stats_all_json);
+  mg_add_uri_handler(server, STATS_JSON_URI, &stats_json);
 
   sprintf(
     tmpBuf,
@@ -27,7 +27,7 @@ void stop_server(void){
   mg_destroy_server(&server);
 }
 
-int stats_all_json(struct mg_connection *conn){
+int stats_json(struct mg_connection *conn){
   sigar_t *sigar;
   sigar_mem_t memory;
   json_object *stats_json, *memory_json;
@@ -39,9 +39,9 @@ int stats_all_json(struct mg_connection *conn){
   stats_json = json_object_new_object();
 
   memory_json = json_object_new_object();
-  json_object_object_add(memory_json, "total", json_object_new_int(memory.ram));
-  json_object_object_add(memory_json, "free", json_object_new_int(memory.free));
-  json_object_object_add(memory_json, "used", json_object_new_int(memory.used));
+  json_object_object_add(memory_json, "total", json_object_new_int64(memory.ram));
+  json_object_object_add(memory_json, "free", json_object_new_int64(memory.free));
+  json_object_object_add(memory_json, "used", json_object_new_int64(memory.used));
 
   json_object_object_add(stats_json, "memory", memory_json);
 
--- src/web.h
+++ src/web.h
@@ -1,8 +1,10 @@
 #define DEFAULT_HTTP_PORT "3000"
 
+#define STATS_JSON_URI "/stats.json"
+
 extern struct mg_server *server;
 
 extern void initialize_server(void);
 extern void stop_server(void);
 
-extern int stats_all_json(struct mg_connection *conn);
+extern int stats_json(struct mg_connection *conn);
