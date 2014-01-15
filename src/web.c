9a3403a6242081d450147d8640a6e311c32a5bd8
Abimael Martell <abimex@gmail.com>
2014-01-14 17:19:48 -0800

    add file system usage

--- src/web.c
+++ src/web.c
@@ -7,6 +7,7 @@ void initialize_server(void){
   server = mg_create_server(NULL);
 
   mg_set_option(server, "listening_port", DEFAULT_HTTP_PORT);
+  mg_set_option(server, "run_as_user", "root");
   mg_set_option(server, "document_root", "public");
 
   mg_add_uri_handler(server, STATS_JSON_URI, &stats_json);
@@ -32,7 +33,10 @@ int stats_json(struct mg_connection *conn){
   sigar_mem_t memory;
   sigar_cpu_list_t cpu_list;
   sigar_cpu_info_list_t cpu_info_list;
-  json_object *stats_json, *memory_json, *cpu_json, *cores_json, *core_json;
+  sigar_file_system_list_t file_system_list;
+  sigar_file_system_usage_t file_system_usage;
+  json_object *stats_json, *memory_json, *cpu_json, *cores_json, *core_json,
+              *file_system_json, *file_systems_json, *file_system_usage_json;
   char *stats_string;
   int i;
 
@@ -48,16 +52,16 @@ int stats_json(struct mg_connection *conn){
 
   json_object_object_add(stats_json, "memory", memory_json);
 
-  cpu_json = json_object_new_object();
   sigar_cpu_info_list_get(sigar, &cpu_info_list);
+  cpu_json = json_object_new_object();
   json_object_object_add(cpu_json, "vendor", json_object_new_string(cpu_info_list.data[0].vendor));
   json_object_object_add(cpu_json, "model", json_object_new_string(cpu_info_list.data[0].model));
   json_object_object_add(cpu_json, "mhz", json_object_new_int(cpu_info_list.data[0].mhz));
   json_object_object_add(cpu_json, "total_cores", json_object_new_int(cpu_info_list.data[0].total_cores));
   sigar_cpu_info_list_destroy(sigar, &cpu_info_list);
 
-  cores_json = json_object_new_array();
   sigar_cpu_list_get(sigar, &cpu_list);
+  cores_json = json_object_new_array();
   for(i = 0; i < cpu_list.number; i++){
     core_json = json_object_new_object();
     json_object_object_add(core_json, "user",  json_object_new_int64(cpu_list.data[i].user));
@@ -68,11 +72,34 @@ int stats_json(struct mg_connection *conn){
     json_object_object_add(core_json, "total", json_object_new_int64(cpu_list.data[i].total));
     json_object_array_add(cores_json, core_json);
   }
-
   json_object_object_add(cpu_json, "cores", cores_json);
-
-
   json_object_object_add(stats_json, "cpu", cpu_json);
+  sigar_cpu_list_destroy(sigar, &cpu_list);
+
+  sigar_file_system_list_get(sigar, &file_system_list);
+  file_systems_json = json_object_new_array();
+  for(i = 0; i < file_system_list.number; i++){
+    file_system_json = json_object_new_object();
+    json_object_object_add(file_system_json, "dir", json_object_new_string(file_system_list.data[i].dir_name));
+    json_object_object_add(file_system_json, "dev", json_object_new_string(file_system_list.data[i].dev_name));
+    json_object_object_add(file_system_json, "type", json_object_new_string(file_system_list.data[i].type_name));
+    json_object_object_add(file_system_json, "sys_type", json_object_new_string(file_system_list.data[i].sys_type_name));
+    json_object_object_add(file_system_json, "options", json_object_new_string(file_system_list.data[i].options));
+
+    file_system_usage_json = json_object_new_object();
+    sigar_file_system_usage_get(sigar, file_system_list.data[i].dir_name, &file_system_usage);
+    json_object_object_add(file_system_usage_json, "use_percent", json_object_new_double(file_system_usage.use_percent));
+    json_object_object_add(file_system_usage_json, "total", json_object_new_int((int) file_system_usage.total));
+    json_object_object_add(file_system_usage_json, "free", json_object_new_int64(file_system_usage.free));
+    json_object_object_add(file_system_usage_json, "used", json_object_new_int64(file_system_usage.used));
+    json_object_object_add(file_system_usage_json, "available", json_object_new_int64(file_system_usage.avail));
+    json_object_object_add(file_system_usage_json, "files", json_object_new_int64(file_system_usage.files));
+    json_object_object_add(file_system_json, "usage", file_system_usage_json);
+
+    json_object_array_add(file_systems_json, file_system_json);
+  }
+  json_object_object_add(stats_json, "file_system", file_systems_json);
+  sigar_file_system_list_destroy(sigar, &file_system_list);
 
   stats_string = (char *) json_object_to_json_string(stats_json);
 
