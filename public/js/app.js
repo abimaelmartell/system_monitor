8b04e3d97758b62a02c671c0aba97b5a708396c1
Abimael Martell <abimex@gmail.com>
2014-01-29 22:34:25 -0800

    remove backbone app, use the SPA pattern instead

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,40 @@
 window.App = window.App || {};
 (function(){
-  var appRouter = new window.App.Router();
-  App.Stats.fetch({
-    success: function(){
-      Backbone.history.start();
+  var SystemMonitor = function(){
+    var _this = this;
+
+    this.template = _.template($("#system-monitor-template").html());
+
+    this.render = function(){
+      $("#main").html(this.template(App.Stats.toJSON()));
+      $("#processes-table, #file-systems-table").dataTable();
+      $("#network-interfaces-table, #cpus-table").dataTable({
+        "bPaginate": false,
+        "bFilter": false
+      });
     }
-  });
+
+    this.initEvents = function(){
+      $("[data-action='refresh-stats']").on("click", function(){
+        var btn = $(this);
+        btn.button('loading');
+        App.Stats.fetch(function(){
+          _this.render();
+          btn.button('reset');
+        })
+      });
+    }
+
+    this.init = function(){
+      App.Stats.fetch(function(){
+        _this.render();
+      })
+    }
+
+    this.initEvents();
+
+    return this.init();
+  }
+
+  window.App.SystemMonitor = new SystemMonitor();
 })();
--- public/js/stats.js
+++ public/js/stats.js
@@ -1,31 +1,44 @@
 window.App = window.App || {};
 (function(){
-  Stats = Backbone.Model.extend({
-    url: "/stats.json",
-    getCPUUsagePercent: function(){
+  Stats = function(){
+    var _this = this;
+
+    this.url = "/stats.json";
+
+    this.fetch = function(success){
+      $.getJSON(this.url, function(response){
+        _this.attributes = response;
+        if(_.isFunction(success))
+          success();
+      });
+    };
+
+    this.getCPUUsagePercent = function(){
       var percent = 0;
       for(core in this.attributes.cpu.cores){
         percent += (100 - ((this.attributes.cpu.cores[core].idle * 100) / this.attributes.cpu.cores[core].total));
       }
       return Math.floor(percent / this.attributes.cpu.total_cores);
-    },
-    getDiskUsagePercent: function(){
+    };
+  
+    this.getDiskUsagePercent = function(){
       var main_fs = _.findWhere(this.attributes.file_systems, {dir: "/"});
       if(main_fs)
         return main_fs.usage.use_percent * 100;
-    },
-    getUptimeString: function(){
-      return App.Utils.parseSecondsToTimeAgo(this.get("uptime"));
-    },
-    toJSON: function(){
+    }
+  
+    this.getUptimeString = function(){
+      return App.Utils.parseSecondsToTimeAgo(this.attributes.uptime);
+    }
+
+    this.toJSON = function(){
       var attributes = _.clone(this.attributes);
       attributes.cpu_usage_percent = this.getCPUUsagePercent();
       attributes.disk_usage_percent = this.getDiskUsagePercent();
       attributes.uptime_string = this.getUptimeString();
-
       return attributes;
     }
-  });
+  }
 
-  App.Stats = new Stats();  
+  App.Stats = new Stats;
 })();
